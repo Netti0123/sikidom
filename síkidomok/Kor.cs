a6124a870e20a822d048f9e36d3234fd86ddd500
Netti0123 <113418004+Netti0123@users.noreply.github.com>
2023-09-20 07:55:48 +0200

    Add files via upload

---
+++ síkidomok/Kor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sikidom
+{
+    internal class Kor : Sikidom
+    {
+        double sugar;
+        public Kor(double sugar) : base("Kör")
+        {
+            this.sugar = sugar;
+        }
+
+        public override double Kerulet()
+        {
+            return Math.Round((2 * this.sugar) * Math.PI, 2);
+        }
+
+        public override double Terulet()
+        {
+            return Math.Round((this.sugar*this.sugar)*Math.PI, 2);
+        }
+    }
+}
